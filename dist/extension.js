90ae488f30ef539ea8936833a8441e19956da316
Yigal Irani <yigal@symbolclick.com>
2025-11-25 16:55:22 -0500

    added launch entry to debug extension. printout to outputChannel

--- dist/extension.js
+++ dist/extension.js
@@ -2,13 +2,14 @@
 import * as vscode from "vscode";
 function activate(context) {
   console.log('Congratulations, your extension "helloworld-sample" is now active!');
-  const outputChannel = vscode.window.createOutputChannel("NPMScriptsMonitor");
+  const outputChannel = vscode.window.createOutputChannel("Scriptsmon");
   vscode.tasks.onDidEndTaskProcess((event) => {
     const task = event.execution.task;
     const exitCode = event.exitCode;
     outputChannel.append(JSON.stringify(event, null, 2));
   });
-  const disposable = vscode.commands.registerCommand("NPMScriptsMonitor.start", async () => {
+  const disposable = vscode.commands.registerCommand("Scriptsmon.start", async () => {
+    outputChannel.append("start");
   });
   context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,14 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "helloworld-sample" is now active!');
-  const outputChannel = vscode.window.createOutputChannel("NPMScriptsMonitor");  
+  const outputChannel = vscode.window.createOutputChannel("Scriptsmon");  
   vscode.tasks.onDidEndTaskProcess((event) => {
     const task = event.execution.task;
     const exitCode = event.exitCode;
     outputChannel.append(JSON.stringify(event,null,2))
   })
-  const disposable = vscode.commands.registerCommand('NPMScriptsMonitor.start',async  () => {
+  const disposable = vscode.commands.registerCommand('Scriptsmon.start',async  () => {
+    outputChannel.append('start')
   });
   context.subscriptions.push(disposable);
 }
