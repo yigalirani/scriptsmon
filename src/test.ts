b7849b29132c9f80742e21622550e5bbdf82894b
Yigal Irani <yigal@symbolclick.com>
2026-01-12 10:58:59 -0500

    upgraded package.json,fixed test to not crash

--- src/test.ts
+++ src/test.ts
@@ -1,16 +1,14 @@
-import {Monitor} from './monitor.js'
+import {read_package_json} from './parser.js'
 import {run_tests} from '@yigal/base_types'
 async function get_package_json_length(){
-  const monitor=new Monitor(['.','..\\tsbase'])
-  await monitor.run()
-  return Object.keys(monitor.root!).length
+  const root=await read_package_json(['.'])
+  return Object.keys(root).length
 }
 if (import.meta.main) {
   void run_tests({
     k:'run on self',
-    v:5,
+    v:8,
     f:get_package_json_length
   })
 }
- 
  
