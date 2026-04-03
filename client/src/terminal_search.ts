1b9b5e63173108b8239c053a7a5b025bbe609e41
Yigal Irani <yigal@symbolclick.com>
2026-04-03 17:01:55 +0300

    added NodeIndex

--- client/src/terminal_search.ts
+++ client/src/terminal_search.ts
@@ -1,4 +1,70 @@
 import {create_element,get_parent_by_class} from './dom_utils.js'
+
+class NodeIndex{
+  text_nodes:Node[]=[]
+  plain_text=''
+  node_offsets:number[]=[]
+  walker
+  constructor(public root: HTMLElement){
+    this.walker=document.createTreeWalker(this.root, NodeFilter.SHOW_TEXT);
+  }
+  iter(){
+    const strings=[]
+    while (this.walker.nextNode()) {
+      const node = this.walker.currentNode;
+      const string=node.textContent||''
+      this.text_nodes.push(node)
+      strings.push(string)
+      this.node_offsets.push(string.length)
+    }
+  }
+  find_node_index_binary(target_index: number): number {
+    let low = 0;
+    let high = this.node_offsets.length - 2;
+    let result = 0;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        
+        // Check if the target falls within this node's range
+        if (this.node_offsets[mid]! <= target_index) {
+            result = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    
+    return result;
+  }
+
+  get_ranges(offsets: [number, number][]): Range[] {
+    const ans= [];
+    for (const [start, end] of offsets) {
+        // Find the starting node using binary search
+        const start_node_idx = this.find_node_index_binary(start);
+        const start_node = this.text_nodes[start_node_idx]!;
+        const start_offset = start - this.node_offsets[start_node_idx]!;
+
+        // Find the ending node (starting search from the start_node_idx for efficiency)
+        let end_node_idx = start_node_idx;
+        const total_nodes = this.text_nodes.length;
+        
+        while (end_node_idx < total_nodes - 1 && this.node_offsets[end_node_idx + 1]! <= end) {
+            end_node_idx++;
+        }
+
+        const end_node = this.text_nodes[end_node_idx]!;
+        const end_offset = end - this.node_offsets[end_node_idx]!;
+        const range = new Range();
+        range.setStart(start_node, start_offset);
+        range.setEnd(end_node, end_offset)
+        ans.push(range)
+    }
+    return ans;
+  }
+}
+
 export class TerminalSearch{
   find_widget
   constructor(private term_el:HTMLElement){
